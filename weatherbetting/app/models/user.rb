d6562c1ad42288d6e42746f8dee963d79bd1dabb
Trevor Stenson <stenson124@gmail.com>
2020-04-28 02:54:32 -0400

    jwt done

--- weatherbetting/app/models/user.rb
+++ weatherbetting/app/models/user.rb
@@ -1,21 +1,4 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable, :recoverable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
-
-  attr_writer :login
-
-  def login
-    @login || self.username || self.email
-  end
-
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
-    end
-  end
-
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
